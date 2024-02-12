dd8fa076f6ef57e6a500da9e49cedf494d55713f
tdev <ktgz14211771329@gmail.com>
2024-02-12 22:14:35 +0700

    edit backend

--- app/service/data.ts
+++ app/service/data.ts
@@ -18,3 +18,26 @@ export async function getUserData(token: string) {
     console.log(e);
   }
 }
+
+export async function getStatsData() {
+  try {
+    const token = localStorage.getItem("token");
+
+    const resp = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}admin/stats`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authtoken: token!,
+      },
+    });
+
+    if (resp.status === 200) {
+      const data = await resp.json();
+      return data;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
